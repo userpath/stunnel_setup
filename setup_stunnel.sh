99a74bde15d85a36c3320bc91b932fcd234ae691
Auston <auston@userpath.co>
2015-04-14 21:52:47 +0000

    :) :)

--- setup_stunnel.sh
+++ setup_stunnel.sh
@@ -52,7 +52,7 @@ then
 fi
 
 echo "$(tput setaf 7)OK, Now we're going to create some ssl keys for stunnel...$(tput sgr0)"
-#cd ~/ && openssl genrsa 1024 > stunnel.key && openssl req -new -x509 -nodes -sha1 -days 365 -key ~/stunnel.key > ~/stunnel.cert && cat ~/stunnel.key ~/stunnel.cert > ~/stunnel.pem
+cd ~/ && openssl genrsa 1024 > stunnel.key && openssl req -new -x509 -nodes -sha1 -days 365 -key ~/stunnel.key > ~/stunnel.cert && cat ~/stunnel.key ~/stunnel.cert > ~/stunnel.pem
 # must stay down here because we just changed directories bro
 StunnelDir=`pwd`
 echo "$(tput setaf 4)Keys created at $StunnelDir/stunnel.key $StunnelDir/stunnel.cert && $StunnelDir/stunnel.pem$(tput sgr0)"
@@ -68,5 +68,5 @@ fi
 ConfExample="${ConfExample//cur_path/$StunnelDir}"
 cd $StunnelDir && echo "$ConfExample" > stunnel.conf
 echo "$(tput setaf 7)Awesome - we just created your stunnel.conf!$(tput sgr0)"
-echo "$(tput setaf 7)To run it: sudo stunnel $StunnelDir/stunnel.conf $(tput sgr0)"
 echo "$(tput setaf 4)DONT FORGET TO START YOUR DEV SERVER ON PORT $Port $(tput sgr0)"
+echo "$(tput setaf 7)To run it: sudo stunnel $StunnelDir/stunnel.conf $(tput sgr0)"
